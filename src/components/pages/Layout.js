de58eb355d8842753a782effaf55eaa6dc7138d5
Danny A. Mase <mase684@gmail.com>
2023-03-25 19:25:37 -1000

    Update Layout.js

--- src/components/pages/Layout.js
+++ src/components/pages/Layout.js
@@ -1,24 +1,15 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-
 const Layout = () => {
 
     return (
         <>
-            <Navbar expand="lg">
-                <Container>
-                    <Navbar.Brand href="/"><span className="text-decor">Pacific</span> Tech</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="react-bs-navbar" />
-                    <Navbar.Collapse id="react-bs-navbar">
-                        <Nav className="me-auto mb-2 mb-lg-0">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/store">Shop</Nav.Link>
-                        </Nav>
-                        <Nav className="ms-auto mb-2 mb-lg-0">
-                            {/* Button shopping cart */}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link active" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/store">Shop</a>
+                </li>
+            </ul>
         </>
     );
 };
